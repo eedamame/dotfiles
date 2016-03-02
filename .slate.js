c3a29de9546bbe3ff88517ac83cdc763f9e1c542
eedamame <eedamame@gmail.com>
2016-03-02 11:12:31 +0900

    slate.jsを再度採用。

--- .slate.js
+++ .slate.js
@@ -23,10 +23,8 @@ var launch_and_focus = function(app_name) {
  
 var key_binds = {};
 key_binds['a:ctrl;cmd'] = launch_and_focus('Google Chrome');
-key_binds['m:ctrl;cmd'] = launch_and_focus('MacVim');
-key_binds['s:ctrl;cmd'] = launch_and_focus('Skype');
-key_binds['d:ctrl;cmd'] = launch_and_focus('Adobe Dreamweaver CS5.5');
-key_binds['i:ctrl;cmd'] = launch_and_focus('iTunes');
+key_binds['v:ctrl;cmd'] = launch_and_focus('MacVim');
+key_binds['s:ctrl;cmd'] = launch_and_focus('Slack');
 key_binds['c:ctrl;cmd'] = launch_and_focus('CotEditor');
 key_binds['t:ctrl;cmd'] = launch_and_focus('iTerm');
 slate.bindAll(key_binds);
